402d1b0d8b53abcf1f31ccfb88998e7a8e0afaf3
Romain Tissier <romain@fedora.home>
2015-03-19 11:51:53 +0100

    conseils de compilation

--- main.c
+++ main.c
@@ -1,67 +1,35 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include <SDL/SDL.h>
 
- 
+void attendre();
 
-void pause();
-
- 
-
-int main(int argc, char *argv[])
-
-{
-
-    SDL_Init(SDL_INIT_VIDEO); // Initialisation de la SDL
-
- 
-
-    SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE); // Ouverture de la fenêtre
-
-    
-
-    pause(); // Mise en pause du programme
-
- 
-
-    SDL_Quit(); // Arrêt de la SDL
-
- 
-
-    return EXIT_SUCCESS; // Fermeture du programme
-
+/**
+ * Fonction main principale, qui instancie l'interface graphique
+ */
+int main(int argc, char *argv[]){
+	// On initialise l'affichage graphique
+    	SDL_Init(SDL_INIT_VIDEO);
+    	SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
+	// On démarre la gestion des événements
+	attendre();
+	// On ferme l'affichage
+    	SDL_Quit();
+   	return EXIT_SUCCESS; 
 }
 
- 
-
-void pause()
-
-{
-
-    int continuer = 1;
-
-    SDL_Event event;
-
- 
-
-    while (continuer)
-
-    {
-
-        SDL_WaitEvent(&event);
-
-        switch(event.type)
-
-        {
-
-            case SDL_QUIT:
-
-                continuer = 0;
-
-        }
-
-    }
-
+/**
+ * Fonction attendre() qui gère les événements de l'interface graphique
+ */
+void attendre(){
+	int test = 1;
+	// On créer un élément de type événement
+	SDL_Event event;
+	// Boucle de traitement des événements
+    	while (test){
+        	SDL_WaitEvent(&event);
+        	switch(event.type){
+            		case SDL_QUIT: test = 0;
+        	}
+    	}
 }
