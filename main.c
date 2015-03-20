3fed5da1414f2a1afb435750c886fcd7c418f6bc
Romain Tissier <romain@fedora.home>
2015-03-20 15:47:27 +0100

    Passage à la SDL 2

--- main.c
+++ main.c
@@ -1,35 +1,36 @@
-#include <stdlib.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
-#include <SDL/SDL.h>
 
-void attendre();
+int main(int argc, char** argv)
+{
+    /* Initialisation simple */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0 )
+    {
+        fprintf(stdout,"Échec de l'initialisation de la SDL (%s)\n",SDL_GetError());
+        return -1;
+    }
 
-/**
- * Fonction main principale, qui instancie l'interface graphique
- */
-int main(int argc, char *argv[]){
-	// On initialise l'affichage graphique
-    	SDL_Init(SDL_INIT_VIDEO);
-    	SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE);
-	// On démarre la gestion des événements
-	attendre();
-	// On ferme l'affichage
-    	SDL_Quit();
-   	return EXIT_SUCCESS; 
-}
+    {
+        /* Création de la fenêtre */
+        SDL_Window* pWindow = NULL;
+        pWindow = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
+                                                                  SDL_WINDOWPOS_UNDEFINED,
+                                                                  640,
+                                                                  480,
+                                                                  SDL_WINDOW_SHOWN);
 
-/**
- * Fonction attendre() qui gère les événements de l'interface graphique
- */
-void attendre(){
-	int test = 1;
-	// On créer un élément de type événement
-	SDL_Event event;
-	// Boucle de traitement des événements
-    	while (test){
-        	SDL_WaitEvent(&event);
-        	switch(event.type){
-            		case SDL_QUIT: test = 0;
-        	}
-    	}
-}
+        //if( pWindow )
+        {
+            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
+
+            SDL_DestroyWindow(pWindow);
+        }
+        else
+        {
+            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
+        }
+    }
+
+    SDL_Quit();
+
+    return 0;}
