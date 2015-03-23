d38314131acd9b5aa9d57ab0ad6ab4077d6edf9a
Romain Tissier <romain@fedora.home>
2015-03-23 19:58:56 +0100

    Ajout bibliographie et commentaire main.c

--- Sources/main.c
+++ Sources/main.c
@@ -1,32 +1,23 @@
 #include <SDL2/SDL.h>
-#include <stdio.h>
 
 int main(int argc, char argv[]){
-	// On initialise la SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0 ){
-      		printf("Erreur SDL: %s", SDL_GetError() );
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 ){	// Initialisation/Test de la SDL
+   		printf("Erreur SDL: %si\n", SDL_GetError() );
        	 	return -1;
     	}
-	// Création d'une fenêtre
-        SDL_Window* fenetre;
-        fenetre = SDL_CreateWindow("Ma première application SDL2",SDL_WINDOWPOS_UNDEFINED,
-                                                                  SDL_WINDOWPOS_UNDEFINED,
-                                                                  640,
-                                                                  480,
-                                                                  SDL_WINDOW_SHOWN);
-
-        if( fenetre )
-        {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voie la fenêtre */
-
-            SDL_DestroyWindow(fenetre);
-        }
-        else
-        {
-            fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
-        }
-    
-
-    SDL_Quit();
-
-    return 0;}
+        SDL_Window* fenetre = SDL_CreateWindow(	// Création d'une fenêtre
+		"Les Lemmings", 		// Titre de la fenêtre
+		SDL_WINDOWPOS_UNDEFINED, 	
+        	SDL_WINDOWPOS_UNDEFINED,
+		640,				// Largeur de la fenêtre
+                480,				// Hauteur de la fenêtre
+                SDL_WINDOW_SHOWN
+	);
+        if(fenetre){				// On vérifie que la fenêtre s'instancie bien
+        	SDL_Delay(3000);  
+        	SDL_DestroyWindow(fenetre);
+        }else
+            printf("Erreur SDL: %s\n",SDL_GetError());
+    	SDL_Quit();
+    	return EXIT_SUCCESS;
+}
