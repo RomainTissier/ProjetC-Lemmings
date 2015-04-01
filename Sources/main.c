59d28bdd20eda0d6cabaea35715cc806b6ccfb04
yocoman31 <yonilevy@hotmail.fr>
2015-04-01 15:51:45 +0200

    Correction
    Corrections orthographiques

--- Sources/main.c
+++ Sources/main.c
@@ -8,30 +8,30 @@ int main(){
     	}
         SDL_Window* fenetre = SDL_CreateWindow(	// Création d'une fenêtre
 		"Les Lemmings", 		// Titre de la fenêtre
-		SDL_WINDOWPOS_UNDEFINED, 	
+		SDL_WINDOWPOS_UNDEFINED,
         	SDL_WINDOWPOS_UNDEFINED,
 		640,				// Largeur de la fenêtre
                 480,				// Hauteur de la fenêtre
                 SDL_WINDOW_SHOWN		// Flag indiquant qu'on ne spécifie pas de comportement en particulier
 	);
         if(fenetre){				// On vérifie que la fenêtre s'instancie bien
-        	
+
 		SDL_Renderer *renderer = NULL;
 	   	SDL_Texture *bitmapTex = NULL;
    		SDL_Surface *bitmapSurface = NULL;
 		renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED);
-		
+
 		bitmapSurface = SDL_LoadBMP("img/hello.bmp");
 		bitmapTex = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
     		SDL_FreeSurface(bitmapSurface);
-		
+
 		SDL_Texture *monPNG = IMG_LoadTexture(renderer, "img/de.png");
 		if (monPNG == NULL){
-			printf("Ereur chargemetn PNG");
+			printf("Erreur chargement PNG");
 		}
 		SDL_Texture *monPNG2 = IMG_LoadTexture(renderer, "img/walker.png");
 		if (monPNG2 == NULL){
-			printf("Ereur chargemetn PNG");
+			printf("Erreur chargement PNG");
 		}
 
 		SDL_Rect dst2;
@@ -44,13 +44,13 @@ int main(){
 		src2.y=0;
 		src2.w=32;
 		src2.h=32;
-		
+
 		SDL_Rect dst;
 		dst.x=100;
 		dst.y=100;
 		dst.w=100;
-		dst.h=100;	
-	
+		dst.h=100;
+
 		SDL_RenderClear(renderer);
         	SDL_RenderCopy(renderer, bitmapTex, NULL, NULL);
 		SDL_RenderCopy(renderer, monPNG,NULL,&dst);
@@ -78,8 +78,8 @@ int main(){
         SDL_BlitSurface(zozor, NULL, ecran, &positionZozor);
         SDL_Flip(ecran);
     }
-		
-		SDL_Delay(3000);  		// On créer une temporisation de 3 secondes 
+
+		SDL_Delay(3000);  		// On crée une temporisation de 3 secondes
 
 		SDL_DestroyTexture(bitmapTex);
     		SDL_DestroyRenderer(renderer);
@@ -89,5 +89,5 @@ int main(){
         }else
             printf("Erreur SDL: %s\n",SDL_GetError());
     	SDL_Quit();				// On ferme la librairie SDL
-    	return EXIT_SUCCESS;	
+    	return EXIT_SUCCESS;
 }
