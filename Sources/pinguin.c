6fc2fc250c5584cf9610bea4824411b4abf2582c
root <root@fedora.home>
2015-04-02 12:03:42 +0200

    Restructuration du code, pinguin qui marche

---
+++ Sources/pinguin.c
@@ -0,0 +1,35 @@
+#include "pinguin.h"
+
+/*Permet de créer et d'initialiser un pinguin*/
+Pinguin* pinguin_create(SDL_Renderer* r){
+	Pinguin *pinguin=malloc(sizeof(Pinguin));
+	pinguin->rendu=r;
+	pinguin->texture=IMG_LoadTexture(r, "img/walker.png");
+	pinguin->spriteCourant.x=0;
+	pinguin->spriteCourant.y=0;
+	pinguin->spriteCourant.h=32;
+	pinguin->spriteCourant.w=32;
+	pinguin->position.x=0;
+	pinguin->position.y=0;
+	pinguin->position.w=32;
+	pinguin->position.h=32;
+	return pinguin;
+}
+
+/*Permet au sprite du pinguin de changer le sens*/
+void pinguin_changerSens(Pinguin* pinguin){
+	if(pinguin->spriteCourant.y==0)
+		pinguin->spriteCourant.y=32;
+	else
+		pinguin->spriteCourant.y=0;
+	pinguin->spriteCourant.x=0;
+}
+
+/*Actualise la position et le sprite du pinguin*/
+//TODO gestion des deux sens
+void pinguin_actualiser(Pinguin* pinguin){
+	pinguin->spriteCourant.x+=32;
+	if(pinguin->spriteCourant.x==256)
+		pinguin->spriteCourant.x=0;
+	pinguin->position.x+=4;
+}
