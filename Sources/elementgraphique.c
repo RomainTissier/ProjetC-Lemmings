42874cbe2c722b7dbb4f92fb4432210f71c3f82b
Romain Tissier <romain@fedora.home>
2015-04-09 11:29:28 +0200

    Détection colision à droite (Pas très propre)

--- Sources/elementgraphique.c
+++ Sources/elementgraphique.c
@@ -1,1 +1,20 @@
 #include "elementgraphique.h"
+
+ElementGraphique* elementgraphique_create(SDL_Renderer* rendu, TypeElement type){
+	if(type==PIC_GLACE){
+		return creerPicGlace(rendu);
+	}
+	return NULL;
+}
+
+static ElementGraphique* creerPicGlace(SDL_Renderer* rendu){
+	ElementGraphique *picGlace= malloc(sizeof(ElementGraphique));	
+	picGlace->rendu=rendu;
+	picGlace->type=PIC_GLACE;
+	picGlace->texture=IMG_LoadTexture(rendu, "img/ice3.png");
+	picGlace->position.x=100;
+	picGlace->position.y=0;
+	picGlace->position.w=10;
+	picGlace->position.h=32;
+	return picGlace;
+}
--- Sources/plateau.c
+++ Sources/plateau.c
@@ -3,6 +3,23 @@
 /*Fonction créant un plateau*/
 Plateau* plateau_creer(SDL_Renderer *rendu){
 	Plateau *plateau=malloc(sizeof(Plateau));
+	plateau->rendu=rendu;
 	plateau->pinguin=pinguin_create(rendu);
+	plateau->picGlace=elementgraphique_create(rendu, PIC_GLACE);
 	return plateau;
+}
+void plateau_rafraichir(Plateau *plateau){
+	pinguin_actualiser(plateau->pinguin);
+	plateau_detecterColision(plateau);
+	SDL_RenderCopy(plateau->rendu, plateau->picGlace->texture, NULL,&(plateau->picGlace->position));
+	SDL_RenderCopy(plateau->rendu, plateau->pinguin->texture, &(plateau->pinguin->spriteCourant), &(plateau->pinguin->position));
+}
+static void plateau_detecterColision(Plateau *plateau){
+	//Detection Verticale 
+		//colision droite
+		if(plateau->pinguin->position.x+plateau->pinguin->position.w >= plateau->picGlace->position.x 
+			&& plateau->pinguin->position.x+plateau->pinguin->position.w < plateau->picGlace->position.x+plateau->picGlace->position.w)
+			pinguin_changerSens(plateau->pinguin);
+		//colision gauche
+	//Detection horizontale
 }
