cfb482ca0d87720527f68953e1066679cf5e1cca
yocoman31 <yonilevy@hotmail.fr>
2015-04-20 10:48:52 +0200

    Collision V2
    Collision verticale et horizontale

--- Sources/dectectioncollision.c
+++ Sources/dectectioncollision.c
@@ -8,30 +8,33 @@ struct AABB{
   int h;
 };
 
-typedef int bool;
-#define true 1
-#define false 0
-
-/*Gestion de la collision entre une boite est un curseur*/
-bool Collision(int curseur_x,int curseur_y,AABB box)
+/*Gestion de la collision entre une boite et un curseur*/
+int Collision(int curseur_x,int curseur_y,AABB box)
 {
    if (curseur_x >= box.x
     && curseur_x < box.x + box.w
     && curseur_y >= box.y
     && curseur_y < box.y + box.h)
-       return true;
+       return 1; //Collision
    else
-       return false;
+       return 0; //Pas collision
 }
 
 /*Gestion de la collision entre deux boites*/
-bool Collision(AABB box1,AABB box2)
+int Collision(AABB box1,AABB box2)
 {
    if((box2.x >= box1.x + box1.w)   // trop à droite
     || (box2.x + box2.w <= box1.x)  // trop à gauche
     || (box2.y >= box1.y + box1.h)  // trop en bas
     || (box2.y + box2.h <= box1.y)) // trop en haut
-          return false;
+        return -1; //Pas de collision
    else
-          return true;
+        if((box2.x < box1.x + box1.w)
+          && (box2.x + box2.w > box1.x)
+          && ((box2.y = box1.y + box1.h -1) || (box2.y + box2.h = box1.y +1)))
+            return 0; //Collision horizontale
+        else if((box2.y < box1.y + box1.h)
+          && (box2.y + box2.h > box1.y)
+          && (box2.x = box1.x + box1.w -1) || (box2.x + box2.w = box1.x +1))
+            return 1; //Collision verticale
 }
