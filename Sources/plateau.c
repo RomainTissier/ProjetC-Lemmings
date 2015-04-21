552eb2a1ffa8ce76de4cc4f24f087411f7142b9c
Romain Tissier <romain@fedora.home>
2015-04-21 13:07:37 +0200

    Début de chute libre!

--- Sources/plateau.c
+++ Sources/plateau.c
@@ -7,9 +7,10 @@ Plateau* plateau_creer(SDL_Renderer *rendu, char niveau[]){
 	plateau->rendu=rendu;
 	plateau->pinguin=pinguin_create(rendu);
 	plateau->pinguin->position.x=50;
-	plateau->elementGraphiques=malloc(2*sizeof(ElementGraphique*));
+	plateau->elementGraphiques=malloc(3*sizeof(ElementGraphique*));
 	plateau->elementGraphiques[0]=elementgraphique_create(rendu, PIC_GLACE,0, 0, 10, 32);
 	plateau->elementGraphiques[1]=elementgraphique_create(rendu, PIC_GLACE,100,0,10,32);
+	plateau->elementGraphiques[2]=elementgraphique_create(rendu, SOL, 0,50,100,10);
 	return plateau;
 }
 //TODO: boucle de destruction pinguin par pinguin
@@ -26,6 +27,7 @@ void plateau_rafraichir(Plateau *plateau){
 	pinguin_actualiser(plateau->pinguin);
 	SDL_RenderCopy(plateau->rendu, plateau->elementGraphiques[0]->texture, NULL,&(plateau->elementGraphiques[0]->position));
 	SDL_RenderCopy(plateau->rendu, plateau->elementGraphiques[1]->texture, NULL,&(plateau->elementGraphiques[1]->position));
+	SDL_RenderCopy(plateau->rendu, plateau->elementGraphiques[2]->texture, NULL,&(plateau->elementGraphiques[2]->position));
 	SDL_RenderCopy(plateau->rendu, plateau->pinguin->texture, &(plateau->pinguin->spriteCourant), &(plateau->pinguin->position));
 }
 
@@ -34,10 +36,13 @@ void plateau_gererCollision(Plateau *plateau){
 	int test=0;
 	int i=0;
 	for(i;i<2;i++){
+		plateau->pinguin->chute=1;
 		if(detecterCollisionRectRect(plateau->elementGraphiques[i]->position,plateau->pinguin->position)==HORIZONTALE){
-			test=1;
+			pinguin_changerSens(plateau->pinguin);
+		}
+		if(detecterCollisionRectRect(plateau->elementGraphiques[i]->position,plateau->pinguin->position)==VERTICALE){
+			printf("Sol détecté\n");
+			plateau->pinguin->chute=0;
 		}
 	}
-	if(test==1)
-		pinguin_changerSens(plateau->pinguin);	
 }
