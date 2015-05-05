b32ea0cd8a7a99a0b271638d5d6f11636f9f8087
Romain Tissier <romain@fedora.home>
2015-05-05 17:29:33 +0200

    Avec les fichiers c est mieux

---
+++ Sources/button.c
@@ -0,0 +1,14 @@
+#include "button.h"
+
+Button * button_create(SDL_Renderer *render,TypeButton type, int x, int y, int w, int h){
+	Button *button=malloc(sizeof(button));
+	button->position.x=x;
+	button->position.y=y;
+	button->position.w=w;
+	button->position.h=h;
+	switch(type){
+		case PAUSE:
+			button->background=IMG_LoadTexture(render,"img/floor.png");
+		break;
+	}
+}
---
+++ Sources/button.h
@@ -0,0 +1,21 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+typedef enum{
+	PAUSE
+}TypeButton;
+
+typedef struct{
+	SDL_Rect position;
+	SDL_Texture *background;
+	unsigned char STATE;
+}Button; 
+
+Button * button_create(SDL_Renderer *render,TypeButton type, int x, int y, int w, int h);
+
+Button * button_updateState(Button button);
+
+#endif
